0e0d594d94e740ca70f8c7e15a19a0397c7c73f4
Joel Miller <jmill53@clemson.edu>
2024-02-20 15:33:53 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -1,57 +1,34 @@
-const https = require('https');
-const fs = require('fs');
+const express = require('express');
 const mysql = require('mysql');
+
+const app = express();
 const config = require('../../config.js');
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: config.host,
     user: config.user,
     password: config.password,
     database: config.database
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL');
 });
 
-const server = https.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/index') { // Updated to handle /about request
-        fs.readFile('index.html', 'utf8', (err, data) => {
-            if (err) {
-                console.error('Error reading HTML file:', err);
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('Internal Server Error');
-                return;
-            }
-            
-            connection.query('SELECT * FROM about_page_data', (err, results) => {
-                if (err) {
-                    console.error('Error querying MySQL database:', err);
-                    res.writeHead(500, {'Content-Type': 'text/plain'});
-                    res.end('Internal Server Error');
-                    return;
-                }
-                
-                const aboutData = results[0];
-                const html = data
-                    .replace('<span id="teamNumber"></span>', `<span id="teamNumber">${aboutData.team_number}</span>`)
-                    .replace('<span id="versionNumber"></span>', `<span id="versionNumber">${aboutData.version_number}</span>`)
-                    .replace('<span id="releaseDate"></span>', `<span id="releaseDate">${aboutData.release_date}</span>`)
-                    .replace('<span id="productName"></span>', `<span id="productName">${aboutData.product_name}</span>`)
-                    .replace('<span id="productDescription"></span>', `<span id="productDescription">${aboutData.product_description}</span>`);
-
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(html);
-            });
-        });
-    }
+app.get('/about', (req, res) => {
+    db.query('SELECT * FROM credentials', (err, result) => {
+        if (err) throw err;
+        res.send(`
+            <h1>About Page</h1>
+            <p>Version #: ${result[0].version}</p>
+            <p>Release Date: ${result[0].release_date}</p>
+            <p>Product Name: ${result[0].product_name}</p>
+            <p>Product Description: ${result[0].product_description}</p>
+        `);
+    });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
