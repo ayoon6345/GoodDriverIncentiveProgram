f37a00affd551d59a664b749fa787992fdbda8fd
Joel Miller <jmill53@clemson.edu>
2024-02-20 18:05:49 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
+const fs = require('fs');
 const app = express();
 const config = require('../../config.js');
-const ejs = require('ejs');
 
 const connection = mysql.createConnection({
   host: config.host,
@@ -19,9 +19,6 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/public');
-
 app.use(express.static('public'));
 
 app.get('/about', (req, res) => {
@@ -32,7 +29,19 @@ app.get('/about', (req, res) => {
       return;
     }
     const aboutData = results[0];
-    res.render('about', { aboutData: aboutData });
+    fs.readFile(__dirname + '/public/about.html', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file: ' + err.stack);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      data = data.replace('${team_number}', aboutData.team_number)
+                 .replace('${version_number}', aboutData.version_number)
+                 .replace('${release_date}', new Date(aboutData.release_date).toLocaleDateString())
+                 .replace('${product_name}', aboutData.product_name)
+                 .replace('${product_description}', aboutData.product_description);
+      res.send(data);
+    });
   });
 });
 
