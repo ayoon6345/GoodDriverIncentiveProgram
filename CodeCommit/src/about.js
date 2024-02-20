c0ba3cf20e65a2b9b73152207f3ecf74f24674bf
Joel Miller <jmill53@clemson.edu>
2024-02-20 17:55:16 +0000

    Updated about.js

--- CodeCommit/src/about.js
+++ CodeCommit/src/about.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <About aboutData={aboutData} />
-    </div>
-  );
-};
+const About = ({ aboutData }) => (
+  <div>
+    <h1>About Us</h1>
+    <p>Team #: {aboutData.team_number}</p>
+    <p>Version #: {aboutData.version_number}</p>
+    <p>Release Date: {new Date(aboutData.release_date).toLocaleDateString()}</p>
+    <p>Product Name: {aboutData.product_name}</p>
+    <p>Product Description: {aboutData.product_description}</p>
+  </div>
+);
 
-export default App;
+export default About;
