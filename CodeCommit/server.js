e1f62faccd6396701600650056866291af1d73d4
Joel Miller <jmill53@clemson.edu>
2024-02-20 19:01:45 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -20,7 +20,7 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
-app.use(express.static(path.join(__dirname, 'dashboard/build')));
+app.use(express.static(path.join(__dirname, '/dashboard/build')));
 
 app.get('/about', (req, res) => {
   connection.query('SELECT * FROM about_page_data', (err, results) => {
@@ -47,7 +47,7 @@ app.get('/about', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dashboard/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/dashboard/build', 'index.html'));
 });
 
 app.listen(3000, () => {
