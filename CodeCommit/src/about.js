bdbe95f28d501ed625ba123d477ac09ddfb606a3
Joel Miller <jmill53@clemson.edu>
2024-02-20 17:26:18 +0000

    Added about.js

---
+++ CodeCommit/src/about.js
@@ -0,0 +1,15 @@
+// About.js
+import React from 'react';
+
+const About = ({ aboutData }) => (
+  <div>
+    <h1>About Us</h1>
+    <p>Team #: {aboutData.team_number}</p>
+    <p>Version #: {aboutData.version_number}</p>
+    <p>Release Date: {new Date(aboutData.release_date).toLocaleDateString()}</p>
+    <p>Product Name: {aboutData.product_name}</p>
+    <p>Product Description: {aboutData.product_description}</p>
+  </div>
+);
+
+export default About;
