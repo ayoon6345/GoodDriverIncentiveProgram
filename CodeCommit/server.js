04d264cf2a3d008d9aede19d72dae21fafdcfce2
Joel Miller <jmill53@clemson.edu>
2024-02-20 17:58:14 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
 const fs = require('fs');
-const path = require('path');
-const About = require('./src/about.js'); // Path to your About component
 const app = express();
 const config = require('../../config.js');
 
@@ -23,7 +19,7 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/about', (req, res) => {
   connection.query('SELECT * FROM about_page_data', (err, results) => {
@@ -33,17 +29,17 @@ app.get('/about', (req, res) => {
       return;
     }
     const aboutData = results[0];
-    const appHtml = ReactDOMServer.renderToString(
-      React.createElement(About, { aboutData: aboutData })
-    );
-    const indexHtmlPath = path.resolve(__dirname, 'public', 'about.html');
-    fs.readFile(indexHtmlPath, 'utf8', (err, data) => {
+    fs.readFile(__dirname + '/public/about.html', 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading file: ' + err.stack);
         res.status(500).send('Internal Server Error');
         return;
       }
-      data = data.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
+      data = data.replace('${team_number}', aboutData.team_number)
+                 .replace('${version_number}', aboutData.version_number)
+                 .replace('${release_date}', new Date(aboutData.release_date).toLocaleDateString())
+                 .replace('${product_name}', aboutData.product_name)
+                 .replace('${product_description}', aboutData.product_description);
       res.send(data);
     });
   });
