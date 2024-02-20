fe446ef133c3e512ecd0862a7236132bae5977ce
Joel Miller <jmill53@clemson.edu>
2024-02-20 17:05:04 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const config = require('../../config.js');
+const ejs = require('ejs');
 
 const connection = mysql.createConnection({
   host: config.host,
@@ -18,10 +19,21 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
 app.use(express.static('public'));
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/about.html');
+  connection.query('SELECT * FROM about_page_data', (err, results) => {
+    if (err) {
+      console.error('Error fetching data: ' + err.stack);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const aboutData = results[0];
+    res.render('about', { aboutData: aboutData });
+  });
 });
 
 app.listen(3000, () => {
