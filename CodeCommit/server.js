fc43c8dc3b463b4b6e79b1a8bd3cd451603022fe
Joel Miller <jmill53@clemson.edu>
2024-02-21 19:21:04 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -20,8 +20,10 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
+// Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'dashboard/build')));
 
+// Route for fetching about page data
 app.get('/about', (req, res) => {
   connection.query('SELECT * FROM about_page_data', (err, results) => {
     if (err) {
@@ -31,16 +33,17 @@ app.get('/about', (req, res) => {
     }
     const aboutData = results[0];
     aboutData.release_date = new Date(aboutData.release_date).toLocaleDateString();
-    // Instead of sending JSON, send the index.html file
+    // Send the index.html file, React Router will handle the route on the client side
     res.sendFile(path.join(__dirname, 'dashboard/build', 'index.html'));
   });
 });
 
-
+// For all other routes, serve the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dashboard/build', 'index.html'));
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
