7e78eefc1f005a7314a52db00e51ce698ab9aa23
Joel Miller <jmill53@clemson.edu>
2024-02-21 19:13:06 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LandingPage from './Landing';
+import AboutUs from './about';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
