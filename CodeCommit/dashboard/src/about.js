fef14ebb4ab6fe72097cc59b6d4e7496b3b27a3f
Joel Miller <jmill53@clemson.edu>
2024-02-22 15:23:08 +0000

    Updated about.js

--- CodeCommit/dashboard/src/about.js
+++ CodeCommit/dashboard/src/about.js
@@ -1,9 +1,16 @@
-// Import React and the CSS file for styling
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-// Define the AboutUs component
-function AboutUs({ team_number, version_number, release_date, product_name, product_description }) {
+function AboutUs() {
+  const [aboutData, setAboutData] = useState({});
+
+  useEffect(() => {
+    fetch('/about')
+      .then(response => response.json())
+      .then(data => setAboutData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div>
       <div className="navbar">
@@ -12,16 +19,14 @@ function AboutUs({ team_number, version_number, release_date, product_name, prod
       </div>
       <div className="container">
         <h1>About Us</h1>
-        <p>Team #: {team_number}</p>
-        <p>Version #: {version_number}</p>
-        <p>Release Date: {release_date}</p>
-        <p>Product Name: {product_name}</p>
-        <p>Product Description: {product_description}</p>
+        <p>Team #: {aboutData.team_number}</p>
+        <p>Version #: {aboutData.version_number}</p>
+        <p>Release Date: {aboutData.release_date}</p>
+        <p>Product Name: {aboutData.product_name}</p>
+        <p>Product Description: {aboutData.product_description}</p>
       </div>
     </div>
   );
 }
 
-// Export the AboutUs component
 export default AboutUs;
-
