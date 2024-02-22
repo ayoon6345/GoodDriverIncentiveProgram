e8d3c72064f68616cb915906b502c13a92c0af02
Joel Miller <jmill53@clemson.edu>
2024-02-22 17:39:06 +0000

    Updated about.js

--- CodeCommit/dashboard/src/about.js
+++ CodeCommit/dashboard/src/about.js
@@ -17,7 +17,7 @@ useEffect(() => {
       <div className="navbar">
         <a href="/">Home</a>
         <a href="/about">About Us</a>
-        <a href="https://team29cpsc4911login.auth.us-east-1.amazoncognito.com/logout?client_id=79svo07u2k8h4oea15mh3krra7&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fteam29.cpsc4911.com%2Fdashboard" className="login-button">Login / Sign Up</a>
+        <a href="https://team29cpsc4911login.auth.us-east-1.amazoncognito.com/logout?client_id=79svo07u2k8h4oea15mh3krra7&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fteam29.cpsc4911.com%2Fdashboard" className="logout-button">Logout</a>
 
       </div>
       <div className="container">
