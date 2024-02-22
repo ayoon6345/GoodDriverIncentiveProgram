0b7fc0428f6cb959c6dbfcf5372ebe4617f15aa9
Joel Miller <jmill53@clemson.edu>
2024-02-22 16:49:52 +0000

    Added Profile.js

---
+++ CodeCommit/dashboard/src/Profile.js
@@ -0,0 +1,13 @@
+// Profile.js
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+      {/* Form for viewing and updating profile details */}
+    </div>
+  );
+}
+
+export default Profile;
