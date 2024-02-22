eb4162f4eae5d0b1ccd2894673344dc5631f2eba
Joel Miller <jmill53@clemson.edu>
2024-02-22 17:38:28 +0000

    Updated Landing.js

--- CodeCommit/dashboard/src/Landing.js
+++ CodeCommit/dashboard/src/Landing.js
@@ -6,7 +6,6 @@ function LandingPage() {
     <div>
       <div className="navbar">
         <a href="/">Home</a>
-        <a href="/about">About Us</a>
         <a href="https://team29cpsc4911login.auth.us-east-1.amazoncognito.com/login?client_id=79svo07u2k8h4oea15mh3krra7&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fteam29.cpsc4911.com%2Fdashboard" className="login-button">Login / Sign Up</a>
       </div>
       <div className="container">
