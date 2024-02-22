ded83eacebcf4eb35b45470a539b3264bfbbca52
Andrew Yoon <ayoon@clemson.edu>
2024-02-22 22:39:48 +0000

    Updated Landing.js

--- CodeCommit/dashboard/src/Landing.js
+++ CodeCommit/dashboard/src/Landing.js
@@ -3,7 +3,7 @@ import './App.css';// Import your CSS for styling
 import './navbar.js';
 function LandingPage() {
   return (  
-    <div style="padding-top:70px;">
+    <div>
       <div className="navbar">
         <a href="/">Home</a>
         <a href="https://team29cpsc4911login.auth.us-east-1.amazoncognito.com/login?client_id=79svo07u2k8h4oea15mh3krra7&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fteam29.cpsc4911.com%2Fdashboard" className="login-button">Login / Sign Up</a>
