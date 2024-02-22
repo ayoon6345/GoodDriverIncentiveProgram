56dc9a2827f22176a9562a444e92df32ea8d7c45
Joel Miller <jmill53@clemson.edu>
2024-02-22 17:23:54 +0000

    Updated server.js

--- CodeCommit/server.js
+++ CodeCommit/server.js
@@ -4,10 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const config = require('../../config.js');
-require('dotenv').config();
-const axios = require('axios');
-const qs = require('querystring');
-
 
 const connection = mysql.createConnection({
   host: config.host,
@@ -39,32 +35,6 @@ app.get('/api/about', (req, res) => {
     res.json(aboutData);
   });
 });
-
-app.get('/oauth2/callback', async (req, res) => {
-  const { code } = req.query;
-  const tokenUrl = process.env.COGNITO_TOKEN_ENDPOINT;
-  const params = {
-    grant_type: 'authorization_code',
-    client_id: process.env.COGNITO_CLIENT_ID,
-    redirect_uri: process.env.COGNITO_REDIRECT_URI,
-    code: code
-  };
-
-  try {
-    const response = await axios.post(tokenUrl, qs.stringify(params), {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-    const { access_token, id_token } = response.data;
-    // Use the tokens as needed
-    res.redirect('/dashboard');
-  } catch (error) {
-    console.error('Error exchanging authorization code for tokens:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
 
 
 // For all other routes, serve the index.html file
