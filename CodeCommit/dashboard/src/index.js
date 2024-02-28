3d01e278635938770fb50edd10fd8cde3bc9868e
Joel Miller <jmill53@clemson.edu>
2024-02-28 16:19:14 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Landing';
 import AboutUs from './about';
-import DriverDashboard from './DriverDashboard'; // Import the dashboard component
+import DriverDashboard from './DriverDashboard';
 import reportWebVitals from './reportWebVitals';
+import { withAuthenticator } from '@aws-amplify/ui-react'; // Import withAuthenticator
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const App = () => (
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<DriverDashboard />} /> {/* Add the dashboard route */}
+        <Route path="/dashboard" element={<DriverDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+const AppWithAuth = withAuthenticator(App); // Wrap the App component with withAuthenticator
+
+ReactDOM.render(<AppWithAuth />, document.getElementById('root'));
 
 reportWebVitals();
