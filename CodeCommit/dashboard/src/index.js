0f697adb220a3f855651cddb593d231bc6cca01c
Joel Miller <jmill53@clemson.edu>
2024-02-28 20:04:43 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Landing';
-import Home from './home';
 import AboutUs from './about';
 import DriverDashboard from './DriverDashboard'; // Import the dashboard component
 import reportWebVitals from './reportWebVitals';
@@ -14,21 +13,6 @@ import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
-// Wrap your components with withAuthenticimport React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './Landing';
-import Home from './home';
-import AboutUs from './about';
-import DriverDashboard from './DriverDashboard'; // Import the dashboard component
-import reportWebVitals from './reportWebVitals';
-
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
-Amplify.configure(config);
 
 // Wrap your components with withAuthenticator
 const AppWithAuth = withAuthenticator(() => (
@@ -36,7 +20,6 @@ const AppWithAuth = withAuthenticator(() => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/dashboard" element={<DriverDashboard />} /> 
       </Routes>
@@ -47,30 +30,6 @@ const AppWithAuth = withAuthenticator(() => (
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppWithAuth /> // Render the AppWithAuth component
-);
-
-reportWebVitals();
-ator
-const AppWithAuth = withAuthenticator(() => (
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<DriverDashboard />} /> 
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route element={<AppWithAuth />} />
-    </Routes>
-  </BrowserRouter>
 );
 
 reportWebVitals();
