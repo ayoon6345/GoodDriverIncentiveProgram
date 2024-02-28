ab72802ebd6d53a4e610512b72aac56ab77f8f7c
Joel Miller <jmill53@clemson.edu>
2024-02-28 19:46:08 +0000

    Updated Landing.js

--- CodeCommit/dashboard/src/Landing.js
+++ CodeCommit/dashboard/src/Landing.js
@@ -15,8 +15,8 @@ function LandingPage() {
     <div>
       <div className="navbar">
         <a href="/">Home</a>
+        <a href="/dashboard">Dashboard</a>
         <a href="/about">About Us</a>
-
         <button onClick={handleSignOut} className="signout-button">Sign Out</button> {/* Add the sign-out button */}
       </div>
       <div className="container">
