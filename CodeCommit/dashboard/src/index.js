5de27baaffb9a0b6be3df7b7eb635b923361f140
Joel Miller <jmill53@clemson.edu>
2024-02-28 17:27:54 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './Landing';
 import AboutUs from './about';
 import DriverDashboard from './DriverDashboard'; // Import the dashboard component
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 import reportWebVitals from './reportWebVitals';
 
-const AuthenticatedApp = withAuthenticator(() => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<AboutUs />} />
-      <Route path="/dashboard" element={<DriverDashboard />} />
-    </Routes>
-    <AmplifySignOut />
-  </BrowserRouter>
-));
+const ProtectedRoute = ({ element, ...rest }) => {
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await Auth.currentAuthenticatedUser();
+      } catch (err) {
+        window.location.href = '/login'; // Redirect to the sign-in page
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  return <Route {...rest} element={element} />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <ProtectedRoute path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/dashboard" element={<DriverDashboard />} />
       </Routes>
