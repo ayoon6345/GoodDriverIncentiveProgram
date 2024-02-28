3d0fee8980be5482a60f278561d9880cbca11531
Joel Miller <jmill53@clemson.edu>
2024-02-28 17:26:07 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './Landing';
 import AboutUs from './about';
 import DriverDashboard from './DriverDashboard'; // Import the dashboard component
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import reportWebVitals from './reportWebVitals';
 
+const AuthenticatedApp = withAuthenticator(() => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/dashboard" element={<DriverDashboard />} />
+    </Routes>
+    <AmplifySignOut />
+  </BrowserRouter>
+));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<DriverDashboard />} /> {/* Add the dashboard route */}
+        <Route path="/dashboard" element={<DriverDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
