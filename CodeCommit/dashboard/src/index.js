5bc8fc1f62960ca58573887907f43f0b9cfe3500
Joel Miller <jmill53@clemson.edu>
2024-02-28 17:06:16 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import './amplify-config';
 import LandingPage from './Landing';
 import AboutUs from './about';
 import DriverDashboard from './DriverDashboard';
 import reportWebVitals from './reportWebVitals';
-import { withAuthenticator } from '@aws-amplify/ui-react'; // Import withAuthenticator
 
 const App = () => (
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/about" element={
+          <AmplifyAuthenticator>
+            <AboutUs />
+            <AmplifySignOut />
+          </AmplifyAuthenticator>
+        } />
         <Route path="/dashboard" element={<DriverDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-const AppWithAuth = withAuthenticator(App, { includeGreetings: true }); // Wrap the App component with withAuthenticator
-
-ReactDOM.render(<AppWithAuth />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
