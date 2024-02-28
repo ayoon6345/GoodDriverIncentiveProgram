47fbcb50876dd7a96e25249724d9deaff3aa159f
Joel Miller <jmill53@clemson.edu>
2024-02-28 19:40:23 +0000

    Updated about.js

--- CodeCommit/dashboard/src/about.js
+++ CodeCommit/dashboard/src/about.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signOut } from 'aws-amplify/auth';
+
 
 function AboutUs() {
   const [aboutData, setAboutData] = useState({});
