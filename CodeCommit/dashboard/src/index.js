13ef0a6d628b2873dc919a0cbb210be0871189c3
Joel Miller <jmill53@clemson.edu>
2024-02-28 17:15:30 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import './amplify-config';
 import LandingPage from './Landing';
 import AboutUs from './about';
-import DriverDashboard from './DriverDashboard';
+import DriverDashboard from './DriverDashboard'; // Import the dashboard component
 import reportWebVitals from './reportWebVitals';
 
-const App = () => (
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={
-          <AmplifyAuthenticator>
-            <AboutUs />
-            <AmplifySignOut />
-          </AmplifyAuthenticator>
-        } />
-        <Route path="/dashboard" element={<DriverDashboard />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/dashboard" element={<DriverDashboard />} /> {/* Add the dashboard route */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
