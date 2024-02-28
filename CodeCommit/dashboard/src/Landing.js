42904b6361caedc24bb077121fe7bdde3798d1a0
Joel Miller <jmill53@clemson.edu>
2024-02-28 19:20:54 +0000

    Updated Landing.js

--- CodeCommit/dashboard/src/Landing.js
+++ CodeCommit/dashboard/src/Landing.js
@@ -15,7 +15,6 @@ function LandingPage() {
     <div>
       <div className="navbar">
         <a href="/">Home</a>
-        <a href="https://team29cpsc4911login.auth.us-east-1.amazoncognito.com/login?client_id=79svo07u2k8h4oea15mh3krra7&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fteam29.cpsc4911.com%2Fdashboard" className="login-button">Login / Sign Up</a>
         <button onClick={handleSignOut} className="signout-button">Sign Out</button> {/* Add the sign-out button */}
       </div>
       <div className="container">
