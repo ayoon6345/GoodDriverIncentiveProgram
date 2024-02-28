763876164c7064babe93f78e787224d959fbb4f3
Joel Miller <jmill53@clemson.edu>
2024-02-28 19:54:30 +0000

    Added home.js

---
+++ CodeCommit/dashboard/src/home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { signOut } from 'aws-amplify/auth';
+
+function HomePage() {
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      // Handle successful sign-out, e.g., redirect to login page
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="navbar">
+        <a href="/home">Home</a>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/about">About Us</a>
+        <button onClick={handleSignOut} className="signout-button">Sign Out</button> {/* Add the sign-out button */}
+      </div>
+      <div className="container">
+        <h1>Good Truck Driver Incentive Program</h1>
+        <p>CPSC 4911 Spring 2024</p>
+        <section className="content">
+          <h1>Sample Home Page</h1>
+        </section>
+      </div>
+      <footer className="footer">
+        <p>&copy; 2024 Good Truck Driver Incentive Program. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
