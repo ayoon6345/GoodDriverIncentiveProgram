5e45cc7ac64e721225057d31fb3e17d29eac0244
Joel Miller <jmill53@clemson.edu>
2024-02-28 20:07:12 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Landing';
 import AboutUs from './about';
+import Home from './home';
 import DriverDashboard from './DriverDashboard'; // Import the dashboard component
 import reportWebVitals from './reportWebVitals';
 
@@ -20,6 +21,7 @@ const AppWithAuth = withAuthenticator(() => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/dashboard" element={<DriverDashboard />} /> 
       </Routes>
