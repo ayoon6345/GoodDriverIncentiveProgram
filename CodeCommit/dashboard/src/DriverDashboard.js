849b2a2704beaeb4f1e61fb3683b41c762e12d84
Joel Miller <jmill53@clemson.edu>
2024-03-04 18:28:16 +0000

    Updated DriverDashboard.js

--- CodeCommit/dashboard/src/DriverDashboard.js
+++ CodeCommit/dashboard/src/DriverDashboard.js
@@ -8,6 +8,8 @@ import Navbar from './navbar'; // Import the Navbar component
 import Profile from './Profile';
 import PointsOverview from './PointsOverview';
 import ProductCatalog from './ProductCatalog';
+import './App.css';
+
 
 Amplify.configure(config);
 
