b3c13de204e7b6047fa7cc520bace1f3dd8ee24a
Joel Miller <jmill53@clemson.edu>
2024-03-04 19:00:18 +0000

    Updated DriverDashboard.js

--- CodeCommit/dashboard/src/DriverDashboard.js
+++ CodeCommit/dashboard/src/DriverDashboard.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Amplify } from 'aws-amplify';
+import React, { useState, useEffect } from 'react';
+import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
@@ -7,18 +7,35 @@ import Navbar from './navbar'; // Import the Navbar component
 import Profile from './Profile';
 import PointsOverview from './PointsOverview';
 import ProductCatalog from './ProductCatalog';
+import AdminDashboard from './AdminDashboard'; // Import the AdminDashboard component
 import './App.css';
-
 
 Amplify.configure(config);
 
 function DriverDashboard() {
   const [activeView, setActiveView] = useState('profile');
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+    const fetchUserType = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        setUserType(user.attributes['custom:user_type']);
+      } catch (error) {
+        console.error('Failed to fetch user type:', error);
+      }
+    };
+
+    fetchUserType();
+  }, []);
 
   const changeView = (view) => {
     setActiveView(view);
   };
 
+  if (userType === 'admin') {
+    return <AdminDashboard />;
+  }
 
   return (
     <div>
