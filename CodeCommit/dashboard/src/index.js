38e7a444c0e6b93bc9c2dac988a1612e9a9d28a6
Joel Miller <jmill53@clemson.edu>
2024-03-04 15:42:46 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Landing';
-import AboutUs from './about';
 import Home from './home';
-import DriverDashboard from './DriverDashboard'; // Import the dashboard component
-import reportWebVitals from './reportWebVitals';
+import AboutUs from './about';
+import DriverDashboard from './DriverDashboard';
 
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
-Amplify.configure(config);
-import Navbar from './navbar';
-const NavBar = ReactDOM.createRoot(document.getElementById('navbar'));
-NavBar.render(
-  <Navbar />
-);
-// Wrap your components with withAuthenticator
-const AppWithAuth = withAuthenticator(() => (
+const App = () => (
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -30,11 +17,9 @@ const AppWithAuth = withAuthenticator(() => (
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-));
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppWithAuth /> // Render the AppWithAuth component
+  <App />
 );
-
-reportWebVitals();
