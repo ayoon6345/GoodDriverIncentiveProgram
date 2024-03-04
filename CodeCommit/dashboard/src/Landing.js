c2d708c1989e2bcc50d67739fa79f9c1a3d3b428
Joel Miller <jmill53@clemson.edu>
2024-03-04 17:43:19 +0000

    Updated Landing.js

--- CodeCommit/dashboard/src/Landing.js
+++ CodeCommit/dashboard/src/Landing.js
@@ -1,20 +1,40 @@
 import React from 'react';
-
+import { Auth } from 'aws-amplify';
 function LandingPage() {
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+      // Handle successful sign-out, e.g., redirect to login page
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  };
+
+  const renderAuthButton = () => {
+    if (Auth.currentAuthenticatedUser()) {
+      return <button onClick={handleSignOut}>Sign Out</button>;
+    } else {
+      return <button onClick={() => Auth.federatedSignIn()}>Sign In</button>;
+    }
+  };
+
   return (
     <div>
-      <div className="navbar">
-      </div>
-      <div className="container">
-        <h1>Good Truck Driver Incentive Program</h1>
-        <p>CPSC 4911 Spring 2024</p>
-        <section className="content">
-          <h1>Sample Landing Page</h1>
-        </section>
-      </div>
-      <footer className="footer">
-        <p>&copy; 2024 Good Truck Driver Incentive Program. All rights reserved.</p>
-      </footer>
+      <body>
+        <div className="navbar">
+          {renderAuthButton()}
+        </div>
+        <div className="container">
+          <h1>TruckStar Rewards</h1>
+          <p>CPSC 4911 Spring 2024</p>
+          <section className="content">
+            <h1>Sample Landing Page</h1>
+          </section>
+        </div>
+        <footer className="footer">
+          <p>&copy; 2024 Good Truck Driver Incentive Program. All rights reserved.</p>
+        </footer>
+      </body>
     </div>
   );
 }
