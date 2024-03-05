1d788bb41cd7e65c830234f1290c5d91ca9d8035
Joel Miller <jmill53@clemson.edu>
2024-03-05 22:29:02 +0000

    Updated navbar.js

--- CodeCommit/dashboard/src/navbar.js
+++ CodeCommit/dashboard/src/navbar.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.css';
-import { signOut } from 'aws-amplify/auth';
+import { signOut, getCurrentUser } from 'aws-amplify/auth';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 
@@ -17,55 +17,60 @@ async function handleSignOut() {
 }
 
 function CustomNavbar() {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    async function fetchCurrentUser() {
+      try {
+        const user = await getCurrentUser();
+        setUsername(user.username);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchCurrentUser();
+  }, []);
 
   return (
     <div className="wrapper">
-      
-    <nav>
-      
-      <div id="logo" onClick={() => {window.location.href='/home'}}>
-      <img src={require('./images/applogo.png')} alt="Logo" width="70px" height="70px" />
-      <h2>company name</h2>      
-      </div>
-      <div id="searchbar">
-        <input type="text" placeholder="Search.." />
-        <button type="submit"><i className="fa fa-search"></i></button>
-      </div>
-  
-      <div className="links">
-  
-        <div className="navlinks" onClick={() => {window.location.href='/about'}}>
-          <h4>about</h4>
+      <nav>
+        <div id="logo" onClick={() => {window.location.href='/home'}}>
+          <img src={require('./images/applogo.png')} alt="Logo" width="70px" height="70px" />
+          <h2>company name</h2>      
         </div>
-  
-        <div className="navlinks">
-          <h4>returns <br /> & orders</h4>
+        <div id="searchbar">
+          <input type="text" placeholder="Search.." />
+          <button type="submit"><i className="fa fa-search"></i></button>
         </div>
-  
-        <div id="pointDiv">
-          <p>points</p>
-          <div id="points">0</div>
+        <div className="links">
+          <div className="navlinks" onClick={() => {window.location.href='/about'}}>
+            <h4>about</h4>
+          </div>
+          <div className="navlinks">
+            <h4>returns <br /> & orders</h4>
+          </div>
+          <div id="pointDiv">
+            <p>points</p>
+            <div id="points">0</div>
+          </div>
+          <div id="cart"> 
+            <img src={require('./images/emptyCart.png')} alt="Cart" width="50px" height="50px" />
+            <h3 id="cartitems">4</h3>
+            <span className="tooltiptext">Cart</span>
+          </div>
+          <div id="profile" onClick={() => {window.location.href='/dashboard'}}>
+            <div><img id="userimg"  src={require('./images/user.png')} alt="user Profile" width="50px" height="50px" /></div>
+            <div id="circle"></div>
+            <span className="tooltiptext">Dashboard</span>
+            {username && <span>{username}</span>}
+          </div>
+          <div className="navlinks" onClick={handleSignOut}>
+            <h4>Sign Out</h4>
+          </div>
         </div>
-        <div id="cart"> 
-          <img src={require('./images/emptyCart.png')} alt="Cart" width="50px" height="50px" />
-          <h3 id="cartitems">4</h3>
-          <span className="tooltiptext">Cart</span>
-        </div>
-  
-        <div id="profile" onClick={() => {window.location.href='/dashboard'}}>
-        <div><img id="userimg"  src={require('./images/user.png')} alt="user Profile" width="50px" height="50px" /></div>
-          <div id="circle"></div>
-          <span className="tooltiptext">Dashboard</span>
-        </div>
-
-        <div className="navlinks" onClick={handleSignOut}>
-        <h4>Sign Out</h4>
-        </div>
-
-
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   );
 }
 
