4440cf936595b79af8aa02c431393c76baf75ca7
Joel Miller <jmill53@clemson.edu>
2024-03-07 14:15:09 +0000

    Updated adminApi.js

--- CodeCommit/dashboard/src/adminApi.js
+++ CodeCommit/dashboard/src/adminApi.js
@@ -1,16 +1,13 @@
-export const addUserToGroup = async (username, groupname) => {
+export const listUsers = async () => {
   try {
-    const response = await fetch('https://lxr0v6oa27.execute-api.us-east-1.amazonaws.com/dev', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, groupname })
+    const response = await fetch('https://lxr0v6oa27.execute-api.us-east-1.amazonaws.com/dev/listUsers', {
+      method: 'GET'
     });
     if (response.ok) {
-      return 'User added to group.';
+      const data = await response.json();
+      return data;
     } else {
-      return 'Failed to add user to group.';
+      return 'Failed to list users.';
     }
   } catch (error) {
     return error.message;
