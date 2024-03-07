c3984de22be4f5a77f1c9167ce44e0fe020b61c5
Joel Miller <jmill53@clemson.edu>
2024-03-07 15:17:05 +0000

    Updated adminApi.js

--- CodeCommit/dashboard/src/adminApi.js
+++ CodeCommit/dashboard/src/adminApi.js
@@ -1,23 +1,3 @@
-export const addUserToGroup = async (username, groupname) => {
-  try {
-    const response = await fetch('https://lxr0v6oa27.execute-api.us-east-1.amazonaws.com/dev', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, groupname })
-    });
-    if (response.ok) {
-      return 'User added to group.';
-    } else {
-      return 'Failed to add user to group.';
-    }
-  } catch (error) {
-    return error.message;
-  }
-};
-
-
 export const listUsers = async () => {
   try {
     const response = await fetch('https://lxr0v6oa27.execute-api.us-east-1.amazonaws.com/dev/listUsers', {
@@ -33,5 +13,3 @@ export const listUsers = async () => {
     return error.message;
   }
 };
-
-// Add similar functions for other Admin Queries API routes
