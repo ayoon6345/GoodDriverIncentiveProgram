22cc96a8c307259f3ce6e1a844d599f323dc1961
Joel Miller <jmill53@clemson.edu>
2024-04-02 14:22:02 +0000

    Updated PointsOverview.js

--- CodeCommit/dashboard/src/PointsOverview.js
+++ CodeCommit/dashboard/src/PointsOverview.js
@@ -1,6 +1,6 @@
+
 import React, { useEffect, useState } from 'react';
 import { Amplify } from 'aws-amplify';
-import { useNavigate } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
@@ -11,34 +11,29 @@ Amplify.configure(config);
 
 function AboutUs() {
   const [userType, setUserType] = useState('');
-  const navigate = useNavigate();
-  const userId = "joelm"; 
-  useEffect(() => {
-    // Placeholder for the actual user ID fetching logic
-    // For example, you might be getting it from the auth session
-    // Replace with actual logic to obtain user ID
 
-    if (userId) {
-      fetch(`/api/getUserType?user_id=${userId}`)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setUserType(data.userType); // Assuming the API returns an object with a userType property
-        })
-        .catch(error => console.error('Error fetching user type:', error));
-    }
-  }, [navigate, userId]); // If userId is dynamic, it needs to be a state or prop
+  useEffect(() => {
+    const userId = "joelm"; // Assuming userId is static for this example
+
+    fetch(`/api/getUserType?user_id=${userId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        setUserType(data);
+      })
+      .catch(error => console.error('Error fetching user type:', error));
+  }, []);
 
   return (
     <div>
-      <Navbar /> {/* Render the Navbar component */}
+      <Navbar />
       <div className="container">
         <h1>About Us</h1>
-        <p>User Type: {userType}</p> {/* Display the fetched user type */}
+        <p>User Type: {userType}</p>
       </div>
     </div>
   );
