64b0fc23966e6097ac5ccac2c7f6a6310270b67b
Joel Miller <jmill53@clemson.edu>
2024-04-02 16:59:32 +0000

    Updated index.js

--- CodeCommit/dashboard/src/index.js
+++ CodeCommit/dashboard/src/index.js
@@ -10,22 +10,20 @@ import ProductSearch from './ProductSearch';
 import { getCurrentUser } from 'aws-amplify/auth'; // Import getCurrentUser
 
 const App = () => {
-  const [userType, setUserType] = useState('');
   const [aboutData, setAboutData] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    async function fetchUserData() {
+    async function fetchCurrentUser() {
       try {
         const user = await getCurrentUser();
         setCurrentUser(user.username);
-        setUserType(user.attributes.find(attr => attr.Name === 'usertype')?.Value || '');
-      } catch (error) {
-        console.error('Error fetching user data:', error);
+      } catch (err) {
+        console.log(err);
       }
     }
 
-    fetchUserData();
+    fetchCurrentUser();
   }, []);
 
   useEffect(() => {
@@ -41,6 +39,7 @@ const App = () => {
   // Filter out the current user from the user list
   const currentUserData = aboutData.find(user => user.user_id === currentUser);
 
+  
   return (
     <BrowserRouter>
       <Routes>
