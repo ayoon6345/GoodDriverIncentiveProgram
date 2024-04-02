c3455099e2d5e4fea4206f4d178ac261a04e3633
Josh Gardner <jgardn8@clemson.edu>
2024-04-02 15:22:08 +0000

    Updated about.js

--- CodeCommit/dashboard/src/about.js
+++ CodeCommit/dashboard/src/about.js
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
-import Navbar from './navbar'; // Import the Navbar component
+import Navbar from './navbar';
 import './App.css';
+import { FaCalendarAlt, FaCode, FaUserFriends, FaInfoCircle } from 'react-icons/fa'; // Importing icons
 
-// AWS Amplify Configuration
 Amplify.configure(config);
 
 function AboutUs() {
@@ -18,7 +18,6 @@ function AboutUs() {
     fetch('/api/about')
       .then(response => {
         if (response.status === 401) {
-          // Redirect to login if not authenticated
           navigate('/login');
           return;
         }
@@ -28,32 +27,38 @@ function AboutUs() {
       .catch(error => console.error('Error fetching data:', error));
   }, [navigate]);
 
+  // Function to select icon based on data type
+  const getIcon = (type) => {
+    switch (type) {
+      case 'team_number':
+        return <FaUserFriends />;
+      case 'version_number':
+        return <FaCode />;
+      case 'release_date':
+        return <FaCalendarAlt />;
+      case 'product_name':
+      case 'product_description':
+        return <FaInfoCircle />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
-      <Navbar /> {/* Render the Navbar component */}
+      <Navbar />
       <div className="about-container">
-        <h1 className="about-header">About Us</h1>
-        <div className="about-content">
-          <div className="about-item">
-            <span className="about-title">Team #:</span>
-            <span className="about-detail">{aboutData.team_number}</span>
-          </div>
-          <div className="about-item">
-            <span className="about-title">Version #:</span>
-            <span className="about-detail">{aboutData.version_number}</span>
-          </div>
-          <div className="about-item">
-            <span className="about-title">Release Date:</span>
-            <span className="about-detail">{aboutData.release_date}</span>
-          </div>
-          <div className="about-item">
-            <span className="about-title">Product Name:</span>
-            <span className="about-detail">{aboutData.product_name}</span>
-          </div>
-          <div className="about-item">
-            <span className="about-title">Product Description:</span>
-            <span className="about-detail">{aboutData.product_description}</span>
-          </div>
+        <h1>About Us</h1>
+        <div className="about-card">
+          {Object.entries(aboutData).map(([key, value]) => (
+            <div key={key} className="about-item">
+              <span className="icon">{getIcon(key)}</span>
+              <div className="about-info">
+                <span className="about-title">{key.replace(/_/g, ' ')}:</span>
+                <span className="about-detail">{value}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
