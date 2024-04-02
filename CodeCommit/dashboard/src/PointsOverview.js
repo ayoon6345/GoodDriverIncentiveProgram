a0b858a36a483af038d25b20caa5add21a4628fc
Joel Miller <jmill53@clemson.edu>
2024-04-02 14:28:00 +0000

    Updated PointsOverview.js

--- CodeCommit/dashboard/src/PointsOverview.js
+++ CodeCommit/dashboard/src/PointsOverview.js
@@ -1,39 +1,34 @@
-
 import React, { useEffect, useState } from 'react';
 import { Amplify } from 'aws-amplify';
+import { useNavigate } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 import Navbar from './navbar'; // Import the Navbar component
 import './App.css';
 
+
 Amplify.configure(config);
 
 function AboutUs() {
-  const [userType, setUserType] = useState('');
+  const [userData, setUserData] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const userId = "joelm"; // Assuming userId is static for this example
-
-    fetch(`/api/getUserType?user_id=${userId}`)
+    fetch('/api/getUsers')
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
+        return response.json();
       })
-      .then(data => {
-        setUserType(data);
-      })
-      .catch(error => console.error('Error fetching user type:', error));
-  }, []);
+      .then(data => setUserData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [navigate]);
 
   return (
     <div>
-      <Navbar />
+      <Navbar /> {/* Render the Navbar component */}
       <div className="container">
         <h1>About Us</h1>
-        <p>User Type: {userType}</p>
+        <p>Team #: {userData.usertype}</p>
       </div>
     </div>
   );
