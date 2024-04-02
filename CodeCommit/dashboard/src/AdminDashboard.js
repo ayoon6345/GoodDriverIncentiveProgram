0e57b78754584b2f0327b8806bc96c9cc7fdaa2b
Joel Miller <jmill53@clemson.edu>
2024-04-02 17:42:26 +0000

    Updated AdminDashboard.js

--- CodeCommit/dashboard/src/AdminDashboard.js
+++ CodeCommit/dashboard/src/AdminDashboard.js
@@ -5,7 +5,6 @@ import { post, get } from 'aws-amplify/api';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import Navbar from './navbar';
 import './App.css';
 
 import amplifyconfig from './amplifyconfiguration.json';
@@ -194,7 +193,6 @@ function DriverDashboard() {
 
   return (
     <div>
-      <Navbar />
       <div className="container">
         <h1>Admin Dashboard</h1>
 
