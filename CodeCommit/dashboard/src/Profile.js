24f948eaed0b9c38e06e12ec0c745a9b08080c80
Joel Miller <jmill53@clemson.edu>
2024-04-02 17:13:42 +0000

    Updated Profile.js

--- CodeCommit/dashboard/src/Profile.js
+++ CodeCommit/dashboard/src/Profile.js
@@ -1,13 +1,61 @@
-// Profile.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrentUser } from 'aws-amplify/auth';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+import Navbar from './navbar'; // Import the Navbar component
+import './App.css';
+import { Amplify } from 'aws-amplify';
+Amplify.configure(config);
 
-function Profile() {
+function PointsOverview() {
+  const [aboutData, setAboutData] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchCurrentUser() {
+      try {
+        const user = await getCurrentUser();
+        setCurrentUser(user.username);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchCurrentUser();
+  }, []);
+
+  useEffect(() => {
+    fetch('/api/getUsers')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // Log the received data
+        setAboutData(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  // Filter out the current user from the user list
+  const currentUserData = aboutData.find(user => user.user_id === currentUser);
+
   return (
     <div>
-      <h2>Profile</h2>
-      {/* Form for viewing and updating profile details */}
+      <Navbar />
+      <div className="container">
+        <h1>User Details</h1>
+        {currentUserData ? (
+          <div>
+            <p>Name: {currentUserData.name}</p>
+            <p>UserName: {currentUserData.user_id}</p>
+            <p>Email: {currentUserData.email}</p>
+            <p>Phone Number: {currentUserData.phone_number}</p>
+            <p>User Type: {currentUserData.usertype}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 }
 
-export default Profile;
+export default PointsOverview;
