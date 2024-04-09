4275f9790c867c36034d4c9221bb27df586191a0
Austin Jameson <adjames@clemson.edu>
2024-04-09 16:00:24 +0000

    Updated SponsorCatalog.js

--- CodeCommit/dashboard/src/SponsorCatalog.js
+++ CodeCommit/dashboard/src/SponsorCatalog.js
@@ -51,6 +51,10 @@ function ChooseItemsForCatalog() {
       });
   }, []);
 
+  const addToCatalog = () => {
+    console.log("adding");
+  }
+
   return (
     <div>
       <h1>Your Catalog</h1>
@@ -62,7 +66,7 @@ function ChooseItemsForCatalog() {
             <p style={{ fontWeight: 'bold' }}>Points: {product.price}</p>
             <p style={{ fontStyle: 'italic' }}>Availability: {product.availability}</p>
             <p>Description: {product.description.length > 100 ? product.description.substring(0, 97) + '...' : product.description}</p>
-            <button onClick={() => onAddToCatalog(product.id,)}>Add to Catalog</button>
+            <button onClick={() => addToCatalog(product.id)}>Add to Catalog</button>
           </div>
         ))}
       </div>
