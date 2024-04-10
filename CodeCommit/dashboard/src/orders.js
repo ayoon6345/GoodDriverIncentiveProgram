8ef96fb0ca24308055f0f7da2d6d6de4f64bd3cc
Andrew Yoon <ayoon@clemson.edu>
2024-04-10 18:38:45 +0000

    Updated orders.js

--- CodeCommit/dashboard/src/orders.js
+++ CodeCommit/dashboard/src/orders.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './navbar'; // Import the Navbar component
 import { getCurrentUser } from 'aws-amplify/auth';
+var newList = [];
 function Orders() {
-    const [cartData, setCartData] = useState([]);
+    const [cartData, setOrderData] = useState([]);
     const [currentUser, setCurrentUser] = useState(null);
   
     useEffect(() => {
@@ -21,21 +22,38 @@ function Orders() {
     }, []);
   
     useEffect(() => {
-      fetch('/api/getCart')
+      fetch('/api/getOrders')
         .then(response => response.json())
         .then(data => {
-          console.log(data); // Log the received data
-          setCartData(data);
+          // Transform the data to match your application's data structure
+          const allItems = data.map((product) => ({
+            id: product.id,
+            user: product.user_id,
+            product: product.product_id,
+          }));
+          setOrderData(allItems) 
         })
         .catch(error => console.error('Error fetching data:', error));
     }, []);
   
     // Filter out the current user from the user list
-    const currentUserData = cartData.find(user => user.user_id === currentUser);
+
+    newList = cartData.filter(function (el) {
+      if ( el.user ===  currentUser) {
+        return el;
+      } 
+    });
   return (
     <div>
         <Navbar /> 
         <div className="container">
+        {newList.map((product) => (
+      <div key={product.id} style={{ width: '300px', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}> 
+        <h3>{product.product_id}</h3>
+
+        <button >Add to Cart</button>
+      </div>
+    ))}
         </div>
     </div>
   );
