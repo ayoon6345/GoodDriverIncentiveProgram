1703157acc2b5aa59edfe05b8b5bd20c1b487955
Andrew Yoon <ayoon@clemson.edu>
2024-04-10 00:35:15 +0000

    Added orders.js to /CodeCommit/dashboard/src

---
+++ CodeCommit/dashboard/src/orders.js
@@ -0,0 +1,45 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './navbar'; // Import the Navbar component
+import { getCurrentUser } from 'aws-amplify/auth';
+function Orders() {
+    const [cartData, setCartData] = useState([]);
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+      async function fetchCurrentUser() {
+        try {
+          const user = await getCurrentUser();
+          setCurrentUser(user.username);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+  
+      fetchCurrentUser();
+    }, []);
+  
+  
+    useEffect(() => {
+      fetch('/api/getCart')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data); // Log the received data
+          setCartData(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+  
+    // Filter out the current user from the user list
+    const currentUserData = cartData.find(user => user.user_id === currentUser);
+  return (
+    <div>
+        <Navbar /> 
+        <div className="container">
+        </div>
+    </div>
+  );
+}
+
+export default Orders;
