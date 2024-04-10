49009f2dcf99f075a1532c6b31738dfa7bc9dc69
Andrew Yoon <ayoon@clemson.edu>
2024-04-10 23:30:22 +0000

    Updated orders.js

--- CodeCommit/dashboard/src/orders.js
+++ CodeCommit/dashboard/src/orders.js
@@ -4,24 +4,28 @@ import './App.css';
 import Navbar from './navbar'; // Import the Navbar component
 import { getCurrentUser } from 'aws-amplify/auth';
 var userOrder = [];
+
 var userOrders = [];
+var productsList = [];
+
+function getProduct(prodID){
+  fetch('https://fakestoreapi.com/products/' + prodID)
+  .then((response) => response.json())
+  .then((data) => {
+    productsList.push(data);     
+    console.log(productsList); 
+  })
+  .catch((error) => {
+    console.error('Error fetching products:', error);
+  });
+}
 
 function Orders() {
 
     const [currentUser, setCurrentUser] = useState(null);
-    var productsList = [];
 
-    function getProduct(prodID){
-      fetch('https://fakestoreapi.com/products/' + prodID)
-      .then((response) => response.json())
-      .then((data) => {
-        productsList.push(data);     
-        console.log(productsList); 
-      })
-      .catch((error) => {
-        console.error('Error fetching products:', error);
-      });
-    }
+
+
     useEffect(() => {
       async function fetchCurrentUser() {
         try {
@@ -57,18 +61,20 @@ function Orders() {
        return el;
       } 
     })
-    console.log(userOrder);
-    userOrder.forEach(function (arrayItem) {
-      getProduct(arrayItem.product);
-  });
-  }
 
+    console.log(userOrder);
+
+    userOrder.forEach(function (arrayItem) {
+        getProduct(arrayItem.product);
+    });
     
+
+  }
   return (
     <div>
         <Navbar /> 
         <div className="container">
-        {userOrder.map((product) => (
+        {productsList.map((product) => (
       <div key={product.id} style={{ width: '300px', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}>
         <h3>{product.product}</h3>
         <button >Add to Cart</button>
