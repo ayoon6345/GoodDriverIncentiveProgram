17aed1a1137e20ef090e50793f92edff97ec0e75
Andrew Yoon <ayoon@clemson.edu>
2024-04-10 23:13:21 +0000

    Updated orders.js

--- CodeCommit/dashboard/src/orders.js
+++ CodeCommit/dashboard/src/orders.js
@@ -68,13 +68,9 @@ function Orders() {
     <div>
         <Navbar /> 
         <div className="container">
-        {productsList.map((product) => (
+        {userOrder.map((product) => (
       <div key={product.id} style={{ width: '300px', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}>
-        <img src={product.image} alt={product.name} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '5px' }} />
-        <h3>{product.name}</h3>
-        <p style={{ fontWeight: 'bold' }}>Points: {product.price}</p>
-        <p style={{ fontStyle: 'italic' }}>Availability: {product.availability}</p>
-        <p>Description: {product.description.length > 100 ? product.description.substring(0, 97) + '...' : product.description}</p>
+        <h3>{product.product}</h3>
         <button >Add to Cart</button>
       </div>
     ))}
