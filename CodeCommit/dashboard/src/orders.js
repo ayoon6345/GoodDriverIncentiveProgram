9aebff719786337694069086be2c10012c67b798
Andrew Yoon <ayoon@clemson.edu>
2024-04-16 00:11:28 +0000

    Updated orders.js

--- CodeCommit/dashboard/src/orders.js
+++ CodeCommit/dashboard/src/orders.js
@@ -77,7 +77,7 @@ function Orders() {
     <div>
         <Navbar /> 
         <div className="container">
-        <h3>{productsList}</h3>
+        <h3>{userOrder}</h3>
         </div>
     </div>
   );
