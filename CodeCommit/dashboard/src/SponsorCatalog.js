e62e00f95d25f6fb0a6fb1075c60cd3c47700e9c
Austin Jameson <adjames@clemson.edu>
2024-04-16 19:41:00 +0000

    Updated SponsorCatalog.js

--- CodeCommit/dashboard/src/SponsorCatalog.js
+++ CodeCommit/dashboard/src/SponsorCatalog.js
@@ -8,6 +8,7 @@ import './App.css';
 import amplifyconfig from './amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
 
+/*
 function GetSponsorCatalog() {
   const [catalogData, setCatalogData] = useState([]);
 
@@ -26,22 +27,36 @@ function GetSponsorCatalog() {
 
   return idsArray;
 }
+*/
 
 
 function ChooseItemsForCatalog() {
   
+  const getSponsorCatalog = () => {
+    const [catalogData, setCatalogData] = useState([]);
+
+    useEffect(() => {
+      fetch('/api/getCatalog/14321')
+        .then(response => response.json())
+        .then(data => {
+          setCatalogData(data);
+          console.log("Catalog data");
+          console.log(catalogData);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+    const idsArray = catalogData.map(product => product.id);
+
+    return idsArray;
+  }
+  
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState([]);
-  const [desiredProductIds, setDesiredProducts] = useState([]);
 
-  
-  useEffect(() => {
-    const data = GetSponsorCatalog();
-    console.log("Desired products");
-    console.log(data);
-    setDesiredProducts(data);
-  }, []);
-
+  const desiredProductIds = getSponsorCatalog();
+  console.log("Desired products");
+  console.log(desiredProductIds);
 
   const addToCatalog = (productId,sponsorId) => {
     console.log("adding" + productId);
