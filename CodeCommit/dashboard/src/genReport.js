8ecf1492057297d524eb72468465b802b1d8cd2f
Joel Miller <jmill53@clemson.edu>
2024-04-16 13:54:53 +0000

    Added genReport.js

---
+++ CodeCommit/dashboard/src/genReport.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import { Amplify } from 'aws-amplify';
+import { fetchAuthSession } from 'aws-amplify/auth';
+import {  get } from 'aws-amplify/api';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+import './App.css';
+
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
+function report() {
+
+
+  async function listAll(limit = 25) {
+    try {
+      const apiName = 'AdminQueries';
+      const path = '/listUsers';
+      const options = {
+        queryStringParameters: { limit: limit },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${(await fetchAuthSession()).tokens.accessToken}`,
+        },
+      };
+      const response = await get({ apiName, path, options });
+      return response;
+    } catch (error) {
+      console.error('Failed to list users:', error);
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    listAll()
+      .then((response) => response.response)
+      .then((result) => result.body.json())
+      .then((data) => {
+        setUsers(data.Users);
+      })
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
+
+
+  return (
+    <div>
+        <div>
+          <h2>Users:</h2>
+          <ul>
+            {users.map((user, index) => (
+              <li key={index}>
+                <div>Username: {user.Username}</div>
+                <div>Name: {user.Attributes.find((attr) => attr.Name === 'name')?.Value}</div>
+                {user.Attributes.map((attribute, attrIndex) => (
+                  <div key={attrIndex}>
+                    {attribute.Name === 'phone_number' && <div>Phone Number: {attribute.Value}</div>}
+                    {attribute.Name === 'email' && <div>Email: {attribute.Value}</div>}
+                  </div>
+                ))}
+                <div>User Status: {user.UserStatus}</div>
+                <div>Enabled: {user.Enabled ? 'Yes' : 'No'}</div>
+                <div>User Create Date: {user.UserCreateDate}</div>
+                <div>User Last Modified Date: {user.UserLastModifiedDate}</div>
+               
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+  );
+}
+
+export default withAuthenticator(report);
