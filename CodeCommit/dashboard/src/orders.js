56613e23464f0ea15f87384c884994d44c46bc1a
Andrew Yoon <ayoon@clemson.edu>
2024-04-16 19:44:33 +0000

    Updated orders.js

--- CodeCommit/dashboard/src/orders.js
+++ CodeCommit/dashboard/src/orders.js
@@ -84,6 +84,9 @@ function Orders() {
           <div key={product.id} style={{ width: '300px', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}>
             <img src={product.image} alt={product.title} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '5px' }} />
             <h3>{product.title}</h3>
+            {userOrder.map((usrOrder) => (
+            <div>{usrOrder.status}</div>
+            ))}
             <p style={{ fontWeight: 'bold' }}>Points: {product.price}</p>
             <p>Description: {product.description.length > 100 ? product.description.substring(0, 97) + '...' : product.description}</p>
           </div>
