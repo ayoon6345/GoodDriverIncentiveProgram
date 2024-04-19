a4331968b038875d4c2c0d1fec3638b856a63afb
Austin Jameson <adjames@clemson.edu>
2024-04-19 15:54:18 +0000

    Updated PointsOverview.js

--- CodeCommit/dashboard/src/PointsOverview.js
+++ CodeCommit/dashboard/src/PointsOverview.js
@@ -27,7 +27,6 @@ function PointsOverview() {
     fetch('/api/getUsers')
       .then(response => response.json())
       .then(data => {
-        console.log(data); // Log the received data
         setAboutData(data);
       })
       .catch(error => console.error('Error fetching data:', error));
