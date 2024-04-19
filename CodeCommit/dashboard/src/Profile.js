08192b9065c8ee0d32cbe30b1aa7be57038ce3d0
Austin Jameson <adjames@clemson.edu>
2024-04-19 15:33:50 +0000

    Updated Profile.js

--- CodeCommit/dashboard/src/Profile.js
+++ CodeCommit/dashboard/src/Profile.js
@@ -51,7 +51,7 @@ function PointsOverview() {
 
   // Filter out the current user from the user list
   const currentUserData = aboutData.find(user => user.user_id === currentUser);
-  const sponsorNames = sponsorArray.filter(s => s.SponsorID === sponsor).map(s => s.SponsorName);
+  const sponsorNames = sponsorArray.filter(s => s.SponsorID === currentUserData.sponsor).map(s => s.SponsorName);
 
   return (
     <div>
