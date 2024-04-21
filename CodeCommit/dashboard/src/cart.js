e0abf575ff13dd933693beaa5eac2f3cb41dac0d
Andrew Yoon <ayoon@clemson.edu>
2024-04-21 23:35:57 +0000

    Updated cart.js

--- CodeCommit/dashboard/src/cart.js
+++ CodeCommit/dashboard/src/cart.js
@@ -3,13 +3,85 @@ import React from 'react';
 import './App.css';
 
 function ShopCart() {
+  var userCarts = [];
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [userCart, setUserCarts] = useState([]);
+
+  async function getProduct(prodId){
+    const response = await fetch('https://fakestoreapi.com/products/'+prodId)
+    const jsonData = await response.json();
+    console.log(jsonData);
+    setProducts(prevProducts => [...prevProducts, jsonData]);
+}
+
+useEffect(() => {
+  async function fetchCurrentUser() {
+    try {
+      const user = await getCurrentUser();
+      setCurrentUser(user.username);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  fetchCurrentUser();
+}, []);
+
+useEffect(() => {
+  if (currentUser !== null) {
+  fetch('/api/getCart')
+    .then(response => response.json())
+    .then(data => {
+      // Transform the data to match your application's data structure
+      userCarts = data.map((product) => ({
+        id: product.id,
+        user: product.user_id,
+        product: product.product_id,
+      }));
+
+      console.log(userCarts);
+      setUserCarts(userCarts.filter(function (el) {
+        if ( el.user ===  currentUser) {
+          return el;
+        } 
+      }))
+      console.log(userCart);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+  }
+}, [currentUser]);
+useEffect(() => {
+  if(userCart.length > 0){
+    userCart.forEach(function (arrayItem) {
+    getProduct(arrayItem.product);  
+    })
+  }
+}, [userCart]);
+  // Filter out the current user from the user list
+
+//userOrder.forEach(function (arrayItem) {
+
+// 
+
+console.log(products);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, React!</h1>
-        <p>This is a simple React page.</p>
-      </header>
+    <div>
+    <Navbar /> 
+    <div className="container">
+
+    {products.map((product) => (
+
+      <div key={product.id} style={{ width: '300px', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}>
+        <img src={product.image} alt={product.title} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '5px' }} />
+        <h3>{product.title}</h3>
+        <p style={{ fontWeight: 'bold' }}>Points: {product.price}</p>
+        <p>Description: {product.description.length > 100 ? product.description.substring(0, 97) + '...' : product.description}</p>
+      </div>
+    ))}
     </div>
+</div>
   );
 }
 
