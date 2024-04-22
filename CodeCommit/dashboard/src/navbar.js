b146ebf1fcad0ed05f6ea718d4c03e8fe73fa909
Andrew Yoon <ayoon@clemson.edu>
2024-04-22 00:22:45 +0000

    Updated navbar.js

--- CodeCommit/dashboard/src/navbar.js
+++ CodeCommit/dashboard/src/navbar.js
@@ -81,7 +81,7 @@ function CustomNavbar() {
             
           <div id="cart" onClick={() => {window.location.href='/cart'}}>
             <img src={require('./images/emptyCart.png')} alt="Cart" width="50px" height="50px" />
-            <h3 id="cartitems">4</h3>
+            <h3 id="cartitems"></h3>
             <span className="tooltiptext">Cart</span>
           </div>
           <div id="profile">
